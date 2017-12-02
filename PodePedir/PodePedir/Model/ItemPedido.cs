11607ae459cc5b42523189da23e29c02f73a66a3
Rodrigo Gonçalves Santana <33496343+rodrigounifafibe@users.noreply.github.com>
2017-12-02 14:25:51 -0200

    Atualização.

---
+++ PodePedir/PodePedir/Model/ItemPedido.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodePedir.Model
+{
+    public class ItemPedido
+    {
+        public long? IdPedido { get; set; }
+        public long? IdProduto { get; set; }
+        public int Quantidade { get; set; }
+        public string Nome { get; set; }
+        public Decimal ValorTotal { get; set; }
+        public string Descricao { get; set; }
+        public Byte[] Foto { get; set; }
+    }
+}
--- PodePedir/PodePedir/View/Pedido/PedidoDetailView.xaml.cs
+++ PodePedir/PodePedir/View/Pedido/PedidoDetailView.xaml.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using PodePedir.Model;
 
 namespace PodePedir.View.Pedido
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
+    
     public partial class PedidoDetailView : ContentPage
     {
+        List<Model.ItemPedido> Lista_pedidos = new List<Model.ItemPedido>();
+        private HttpClient client = new HttpClient();
+
         public PedidoDetailView()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //Se for diferente de nulo é porque tem cliente para ser exibido
+            if(Global.GCliente != null)
+            {
+                txtCliente.Text = Global.GCliente.Nome;
+            }
+
+            if(Global.GProduto != null)
+            {
+                txtProduto.Text = Global.GProduto.Nome;
+            }
         }
     }
 }
